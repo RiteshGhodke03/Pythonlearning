e4237dfc02616950202db383601db7afe870c83f
Ritesh Ghodke <Riteshrg03@gmail.com>
2025-01-15 13:05:46 +0530

    Min max

---
+++ Jan/ex_14012025_Operators/Lab_014_power_operator.py
@@ -0,0 +1,18 @@
+print(2 ** 2) #power **
+
+#compare operator
+print(2 == 2)
+print(3 == 2)
+
+#increment decrement operators
+
+x = 5
+
+x += 1 #increment operator
+
+print(x)
+
+x -= 1 #decrement operator
+
+print(x)
+
